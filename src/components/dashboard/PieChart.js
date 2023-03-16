94e431fdfd15dd6d321ea26bcbd1374209822b54
sangeethajyothilingam <sangeethajoe2007@gmail.com>
2023-03-16 11:16:41 +0530

    first commit

--- src/components/dashboard/PieChart.js
+++ src/components/dashboard/PieChart.js
@@ -1,4 +1,3 @@
-
 /*doughnutchart*/
 import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
@@ -7,25 +6,19 @@ import { Doughnut } from "react-chartjs-2";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-    labels: ["Male", "Female"],
-    datasets: [
-        {
-            label: "# of Votes",
-            data: [40, 60],
-            backgroundColor: [
-
-                "rgba(28,200,138,1)",
-                "rgba(54,185,204,1)"
-            ],
-            borderColor: "white",
-            borderwidth: 500,
-            cutout: "75%"
-        }
-    ]
+  labels: ["Male", "Female"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [55, 45],
+      backgroundColor: ["orange", "green"],
+      borderColor: "white",
+      borderwidth: 500,
+      cutout: "75%",
+    },
+  ],
 };
 
 export function PieChart() {
-    return <Doughnut data={data} />;
+  return <Doughnut data={data} />;
 }
-
-
--- src/components/members/MembersList.js
+++ src/components/members/MembersList.js
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 function MembersList({ memberdata }) {
-
-
-    return (
-        <>
-            <tr>
-                <td>{memberdata.id}</td>
-                <td>{memberdata.name}</td>
-                <td>{memberdata.email}</td>
-                <td>{memberdata.contact}</td>
-                <td><Link className='btn btn-info btn-sm me-1' to={`/portal/memberview/${memberdata.id}`} >view</Link>
-                </td>
-            </tr>
-        </>
-    )
+  return (
+    <>
+      <tr>
+        <td>{memberdata.id}</td>
+        <td>{memberdata.name}</td>
+        <td>{memberdata.email}</td>
+        <td>{memberdata.contact}</td>
+        <td>
+          <Link
+            className="btn btn-info btn-sm me-1"
+            to={`/portal/memberview/${memberdata.id}`}
+          >
+            <FontAwesomeIcon icon={faEye} color="#810CA8" />
+          </Link>
+        </td>
+      </tr>
+    </>
+  );
 }
 
-export default MembersList
+export default MembersList;
